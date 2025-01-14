e75ab7ba1313c79917d2b4455f18def76d598ab0
Shaun Culver <shaun@culver.ch>
2025-01-14 11:45:33 +0200

    Remove comments

--- DesignPatterns/AppleStoreVisitor/Sources/VisitorKata.swift
+++ DesignPatterns/AppleStoreVisitor/Sources/VisitorKata.swift
@@ -1,24 +1,18 @@
-// In addition to the current concrete visitors, add a sales tax visitor that calculates the sales tax for a product.
-// The sales tax should be a variable initialised with the class.
-// Write tests to verify the sales tax visitor for the `MacBookProProduct` and `VisionProProduct`.
-// Test the new visitor with 10% and 15% sales tax rates. (Two tests)
-// Include previous tests.
-
 struct Kata {
-    
+
     // Visitor
-    
+
     protocol Visitor {
         associatedtype VisitorResult
         func visit(_ product: Product) -> VisitorResult
     }
-    
+
     // Concrete Visitor
-    
+
     class SalesTaxVisitor {
         // Kata
     }
-    
+
     class EducationDiscountVisitor: Visitor {
         typealias VisitorResult = Double
         private let discountPercentage = 0.25
@@ -44,9 +38,9 @@ struct Kata {
             "Product with ID \(product.id) costs $\(product.price)"
         }
     }
-    
+
     // Element
-    
+
     protocol Product {
         var id: String { get }
         var price: Double { get }
@@ -55,13 +49,13 @@ struct Kata {
     protocol VisitorAccepting {
         func accept<V: Visitor>(_ visitor: V) -> V.VisitorResult
     }
-    
+
     // Concrete Element
-    
+
     struct MacBookProProduct: Product, VisitorAccepting {
         let id: String
         let price: Double
-        
+
         init(id: String, price: Double) {
             self.id = id
             self.price = price
@@ -80,7 +74,7 @@ struct Kata {
             self.id = id
             self.price = price
         }
-        
+
         func accept<V: Visitor>(_ visitor: V) -> V.VisitorResult {
             visitor.visit(self)
         }
