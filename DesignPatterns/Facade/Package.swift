d7a5c1a084f14f07a6159c40038c70a3f53cf721
Shaun Culver <shaun@culver.ch>
2025-02-03 08:43:10 +0200

    Facade package update

--- DesignPatterns/Facade/Package.swift
+++ DesignPatterns/Facade/Package.swift
@@ -3,22 +3,56 @@
 import PackageDescription
 
 let package = Package(
-    name: "AppleStoreFacade",
+    name: "Facade",
     platforms: [
         .macOS(.v14),
-        .iOS(.v17)
+        .iOS(.v17),
     ],
     products: [
         .library(
-            name: "AppleStoreFacade",
-            targets: ["AppleStoreFacade"]),
+            name: "Facade",
+            targets: [
+                "FacadeArticleCode",
+                "FacadeArticleTests",
+                "FacadeKataCode",
+                "FacadeKataTests",
+                "FacadeKataSolutionCode",
+                "FacadeKataSolutionTests",
+            ])
     ],
     targets: [
         .target(
-            name: "AppleStoreFacade"),
+            name: "FacadeArticleCode",
+            path: "Article",
+            sources: ["FacadeArticleCode.swift"]
+        ),
         .testTarget(
-            name: "AppleStoreFacadeTests",
-            dependencies: ["AppleStoreFacade"]
+            name: "FacadeArticleTests",
+            dependencies: ["FacadeArticleCode"],
+            path: "Article",
+            sources: ["FacadeArticleTests.swift"]
+        ),
+        .target(
+            name: "FacadeKataCode",
+            path: "Kata",
+            sources: ["FacadeKataCode.swift"]
+        ),
+        .testTarget(
+            name: "FacadeKataTests",
+            dependencies: ["FacadeKataCode"],
+            path: "Kata",
+            sources: ["FacadeKataTests.swift"]
+        ),
+        .target(
+            name: "FacadeKataSolutionCode",
+            path: "KataSolution",
+            sources: ["FacadeKataSolutionCode.swift"]
+        ),
+        .testTarget(
+            name: "FacadeKataSolutionTests",
+            dependencies: ["FacadeKataSolutionCode"],
+            path: "KataSolution",
+            sources: ["FacadeKataSolutionTests.swift"]
         ),
     ]
 )
