4a992528dace797d13cdb44309e1f4b2d5c4abe9
Shaun Culver <shaun@culver.ch>
2025-02-04 09:05:58 +0200

    Observer fixes

--- DesignPatterns/Observer/Article/ObserverArticleTests.swift
+++ DesignPatterns/Observer/Article/ObserverArticleTests.swift
@@ -7,12 +7,12 @@ import Testing
     let notifier = WebSocketBagNotifier()
     let bagListViewModel = BagListViewModel(notifier: notifier)
     let bagIconViewModel = BagIconViewModel(notifier: notifier)
-    
+
     #expect(bagListViewModel.products.isEmpty)
     #expect(bagIconViewModel.badgeCount == 0)
 
-    Mocks.oneProduct.forEach { notifier.addProduct($0) }
-    
+    Stub.oneProduct.forEach { notifier.addProduct($0) }
+
     #expect(bagListViewModel.products.count == 1)
     #expect(bagIconViewModel.badgeCount == 1)
 }
@@ -21,41 +21,41 @@ import Testing
     let notifier = WebSocketBagNotifier()
     let bagListViewModel = BagListViewModel(notifier: notifier)
     let bagIconViewModel = BagIconViewModel(notifier: notifier)
-    
+
     #expect(bagListViewModel.products.isEmpty)
     #expect(bagIconViewModel.badgeCount == 0)
 
-    Mocks.twoProducts.forEach { notifier.addProduct($0) }
-    
+    Stub.twoProducts.forEach { notifier.addProduct($0) }
+
     #expect(bagListViewModel.products.count == 2)
     #expect(bagIconViewModel.badgeCount == 2)
 }
 
-struct Mocks {
+struct Stub {
     static let product1ID = UUID(uuidString: "11111111-1111-1111-1111-111111111111")!
     static let product2ID = UUID(uuidString: "22222222-2222-2222-2222-222222222222")!
     static let product3ID = UUID(uuidString: "33333333-3333-3333-3333-333333333333")!
-    
+
     static var product1: Product {
         Product(id: product1ID, name: "iPad Pro", price: 999.99)
     }
-    
+
     static var product2: Product {
         Product(id: product2ID, name: "Apple Pencil", price: 99.99)
     }
-    
+
     static var product3: Product {
         Product(id: product3ID, name: "Smart Keyboard", price: 169.99)
     }
-    
+
     static var oneProduct: [Product] {
         [product1]
     }
-    
+
     static var twoProducts: [Product] {
         [product1, product2]
     }
-    
+
     static var threeProducts: [Product] {
         [product1, product2, product3]
     }
